56168314a747fe30263a856e87242a7f29c0d54b
Fırat Doğan <firatdogan0@gmail.com>
2021-08-13 00:09:00 +0300

    error fixing

--- socket/disconnect.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = socketUpdate = (socket) => socket.on('disconnecting', (reason) => {
-
-    global.socketUsers = global.socketUsers.filter(e => e.socketId != socket.id)
-    console.log('\x1b[31m', 'disconnected =>', socket.id, reason, '\x1b[0m')
-
-
-
-});
