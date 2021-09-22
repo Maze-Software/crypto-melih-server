84aeee44b90a7c741834acfff2893fd70ff76998
Fırat Doğan <firatdogan0@gmail.com>
2021-09-22 21:15:29 +0300

    trading view

--- controllers/tradingview.js
+++ controllers/tradingview.js
@@ -10,17 +10,18 @@ const User = require("../schemas/user");
 const TradingViews = require("../schemas/tradingview");
 const Prices = require("../schemas/prices");
 
+// 0 - hiç girilmemşi 1- Girilmiş ama onay bekliyor, 2- Onaylanmış- 3- Onaylanmamış 4- Subscribe bile değil
 const getStatus = async (req, res) => {
   const getUser = await checkLogin(req)
   if (await !getUser) { return new errorHandler(res, 401, -1) }
-  if (await !isUserSubscribed(getUser)) { return new errorHandler(res, 401, -1) }
+  if (await isUserSubscribed(getUser) == false) { return res.send({ status: 4 }) }
 
-  const findRecord = await TradingViews.find({ userId: getUser._id })
+  const findRecord = await TradingViews.findOne({ userId: getUser._id })
   if (!findRecord) {
     return res.send({ status: 0 })
   }
   else {
-    return res.send({ status: findRecord[0].status })
+    return res.send({ status: findRecord.status })
   }
 }
 const apply = async (req, res) => {
@@ -47,7 +48,6 @@ const approve = async (req, res) => {
   }
   return new errorHandler(res, 500, 0)
 }
-
 
 const remove = async (req, res) => {
   if (await !isAdmin(req)) { return new errorHandler(res, 401, -1) }
