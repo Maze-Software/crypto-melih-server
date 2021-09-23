d03b167bd5d1b8cb7b01d574c0f0d1fdee10b2ea
Fırat Doğan <firatdogan0@gmail.com>
2021-09-23 15:01:34 +0300

    news

--- controllers/news.js
+++ controllers/news.js
@@ -4,7 +4,7 @@ const errorHandler = require('./errorhandler');
 
 const getNews = async (req, res) => {
     if (await !checkLogin(req)) { return new errorHandler(res, 401, -1) }
-    res.send({ data: await News.find({}).sort({ created_at: -1 }) })
+    res.send({ data: await News.find({}).sort({ createdAt: -1 }) })
 }
 
 const addNews = async (req, res) => {
