d1fe98e7e06c3f3e0e4bc621d5925513b8e6875f
Fırat Doğan <firatdogan0@gmail.com>
2021-09-26 22:58:02 +0300

    fix 2

--- routes/buy.js
+++ routes/buy.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const route = express.Router();
-const { checkLogin } = require('../controllers/general')
+const { checkLogin, activeUserSubscription } = require('../controllers/general')
 
 
 route.post('/', async (req, res) => {
     if (req.body.userId) {
         const { month = 1 } = req.body
-        await general.activeUserSubscription(month, req.body.userId);
+        await activeUserSubscription(month, req.body.userId);
         res.send(true)
     }
     else {
@@ -20,7 +20,7 @@ route.post('/purchase', async (req, res) => {
     const user = await checkLogin(req)
     if (user && req.body.key == "uygulamaicisatinalma") {
         const month = 1
-        await general.activeUserSubscription(month, req.body.userId);
+        await activeUserSubscription(month, req.body.userId);
         res.send(true)
     }
     else {
