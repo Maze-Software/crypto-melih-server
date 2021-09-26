d7917e12e54accd5bef750f41e5394b8da9bdabf
Fırat Doğan <firatdogan0@gmail.com>
2021-09-26 22:56:40 +0300

    subscription url

--- routes/buy.js
+++ routes/buy.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const route = express.Router();
-
+const { checkLogin } = require('../controllers/general')
 
 
 route.post('/', async (req, res) => {
@@ -15,6 +15,19 @@ route.post('/', async (req, res) => {
     else {
         res.send(false)
     }
+});
+route.post('/pruchase', async (req, res) => {
+    const user = await checkLogin(req)
+    if (user && req.body.key == "uygulamaicisatinalma") {
+        const month = 1
+        await general.activeUserSubscription(month, req.body.userId);
+        res.send(true)
+    }
+    else {
+        res.status(500).send("fail")
+    }
+
+
 });
 
 
